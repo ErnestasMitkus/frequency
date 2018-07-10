f9a313688eb237473590758da3b326c953939a58
Ernestas Mitkus <ernestas@zenitech.co.uk>
2018-07-10 09:43:16 +0300

    Update yml and poms

--- frequency-api/src/main/java/org/zenitech/frequencyapi/controller/ApiController.java
+++ frequency-api/src/main/java/org/zenitech/frequencyapi/controller/ApiController.java
@@ -2,12 +2,24 @@ package org.zenitech.frequencyapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.zenitech.frequencyapi.client.FrequencyServiceClient;
 import reactor.core.publisher.Mono;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
 
 @RestController
 public class ApiController {
@@ -35,11 +47,29 @@ public class ApiController {
         return "uploading";
     }
 
-    @PostMapping("/upload")
+    @PostMapping(value = "/upload", consumes = "text/plain")
     public Callable<String> uploadFiles(@RequestPart("uploadingFiles") MultipartFile[] uploadingFiles) {
         for (MultipartFile uploadedFile : uploadingFiles) {
             System.out.println(uploadedFile.getName());
         }
+        List<String> lines = Arrays.stream(uploadingFiles)
+                .map(it -> {
+                    try {
+                        return it.getInputStream();
+                    } catch (IOException e) {
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
+                .map(it -> new InputStreamReader(it, StandardCharsets.UTF_8))
+                .map(BufferedReader::new)
+                .map(BufferedReader::lines)
+                .flatMap(it -> it)
+                .map(frequencyServiceClient::echo)
+                .collect(Collectors.toList());
+
+        System.out.println(lines);
+
         // convert files to array of lines
         // for each line call service to count frequency
         // merge frequencies with map reduce
